f6e1b23a181cda1ce4e4ba6f7bcc245edaab35f9
Alejandro Serrano <trupill@gmail.com>
2019-10-02 11:25:59 +0200

    First working version of mu-grpc + example

--- grpc/src/Mu/Server/GRpc.hs
+++ grpc/src/Mu/Server/GRpc.hs
@@ -11,38 +11,71 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
 import Data.Kind
 import Data.Proxy
+import Network.GRPC.HTTP2.Encoding (uncompressed, gzip)
 import Network.GRPC.Server.Wai (ServiceHandler)
 import Network.GRPC.Server.Handlers.NoLens
+import Network.GRPC.Server.Wai as Wai
+import Network.Wai (Application)
+import Network.Wai.Handler.Warp (Port, Settings, run, runSettings)
+import Network.Wai.Handler.WarpTLS (TLSSettings, runTLS)
 
 import Mu.Rpc
 import Mu.Server
 import Mu.Schema
 import Mu.Schema.Adapter.ProtoBuf
 
-gRpcServer :: forall name methods handlers.
-              (KnownName name, GRpcServiceHandlers methods handlers)
-           => ByteString -> ServerIO ('Service name methods) handlers
-           -> [ServiceHandler]
-gRpcServer p (Server svr) = gRpcServiceHandlers p serviceName svr
+runGRpcApp
+  :: (KnownName name, GRpcMethodHandlers methods handlers)
+  => Port -> ByteString -> ServerIO ('Service name methods) handlers
+  -> IO ()
+runGRpcApp port pkgName svr = run port (gRpcApp pkgName svr)
+
+runGRpcAppSettings
+  :: (KnownName name, GRpcMethodHandlers methods handlers)
+  => Settings -> ByteString -> ServerIO ('Service name methods) handlers
+  -> IO ()
+runGRpcAppSettings st pkgName svr = runSettings st (gRpcApp pkgName svr)
+
+runGRpcAppTLS
+  :: (KnownName name, GRpcMethodHandlers methods handlers)
+  => TLSSettings -> Settings
+  -> ByteString -> ServerIO ('Service name methods) handlers
+  -> IO ()
+runGRpcAppTLS tls st pkgName svr = runTLS tls st (gRpcApp pkgName svr)
+
+gRpcApp
+  :: (KnownName name, GRpcMethodHandlers methods handlers)
+  => ByteString -> ServerIO ('Service name methods) handlers
+  -> Application
+gRpcApp p svr = Wai.grpcApp [uncompressed, gzip]
+                            (gRpcServiceHandlers p svr)
+
+gRpcServiceHandlers
+  :: forall name methods handlers.
+     (KnownName name, GRpcMethodHandlers methods handlers)
+  => ByteString -> ServerIO ('Service name methods) handlers
+  -> [ServiceHandler]
+gRpcServiceHandlers p (Server svr) = gRpcMethodHandlers p serviceName svr
   where serviceName = BS.pack (nameVal (Proxy @name))
         
-class GRpcServiceHandlers (ms :: [Method mnm]) (hs :: [Type]) where
-  gRpcServiceHandlers :: ByteString -> ByteString
-                      -> HandlersIO ms hs -> [ServiceHandler]
+class GRpcMethodHandlers (ms :: [Method mnm]) (hs :: [Type]) where
+  gRpcMethodHandlers :: ByteString -> ByteString
+                     -> HandlersIO ms hs -> [ServiceHandler]
 
-instance GRpcServiceHandlers '[] '[] where
-  gRpcServiceHandlers _ _ H0 = []
-instance (KnownName name, GRpcServiceHandler args r h, GRpcServiceHandlers rest hs)
-         => GRpcServiceHandlers ('Method name args r ': rest) (h ': hs) where
-  gRpcServiceHandlers p s (h :<|>: rest)
-    = gRpcServiceHandler (Proxy @args) (Proxy @r) (RPC p s methodName) h
-      : gRpcServiceHandlers p s rest
+instance GRpcMethodHandlers '[] '[] where
+  gRpcMethodHandlers _ _ H0 = []
+instance (KnownName name, GRpcMethodHandler args r h, GRpcMethodHandlers rest hs)
+         => GRpcMethodHandlers ('Method name args r ': rest) (h ': hs) where
+  gRpcMethodHandlers p s (h :<|>: rest)
+    = gRpcMethodHandler (Proxy @args) (Proxy @r) (RPC p s methodName) h
+      : gRpcMethodHandlers p s rest
     where methodName = BS.pack (nameVal (Proxy @name))
 
-class GRpcServiceHandler args r h where
-  gRpcServiceHandler :: Proxy args -> Proxy r -> RPC -> h -> ServiceHandler
+class GRpcMethodHandler args r h where
+  gRpcMethodHandler :: Proxy args -> Proxy r -> RPC -> h -> ServiceHandler
 
 instance (HasProtoSchema vsch vty v, HasProtoSchema rsch rty r)
-         => GRpcServiceHandler '[ 'ArgSingle '(vsch, vty) ] ('RetSingle '(rsch, rty)) (v -> IO r) where
-  gRpcServiceHandler _ _ rpc h
+         => GRpcMethodHandler '[ 'ArgSingle vsch vty ] ('RetSingle rsch rty)
+                              (v -> IO r) where
+  gRpcMethodHandler _ _ rpc h
     = unary (fromProtoViaSchema @vsch, toProtoViaSchema @rsch) rpc (\_req -> h)
