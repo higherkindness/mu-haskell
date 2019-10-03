de04aad06c9f46822b2bbed523ef75b4b4b0b368
Alejandro Serrano <trupill@gmail.com>
2019-10-03 11:49:15 +0200

    Example of deployment with Docker

---
+++ deployment-examples/docker/Main.hs
@@ -0,0 +1,10 @@
+{-# language OverloadedStrings #-}
+module Main where
+
+import Mu.Server.GRpc
+import Mu.Rpc.Examples
+
+main :: IO ()
+main = do 
+  putStrLn "running quickstart application"
+  runGRpcApp 8080 "helloworld" quickstartServer
