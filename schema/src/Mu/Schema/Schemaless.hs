019c6d095b1b286ada12abdeab1af91474007857
Alejandro Serrano <trupill@gmail.com>
2019-10-08 12:53:54 +0200

    Conversion from schemaless to schema

--- schema/src/Mu/Schema/Schemaless.hs
+++ schema/src/Mu/Schema/Schemaless.hs
@@ -2,7 +2,8 @@
              KindSignatures, ScopedTypeVariables,
              TypeApplications, TypeOperators,
              FlexibleContexts, MultiParamTypeClasses,
-             AllowAmbiguousTypes, StandaloneDeriving #-}
+             AllowAmbiguousTypes, StandaloneDeriving,
+             FlexibleInstances, UndecidableInstances #-}
 module Mu.Schema.Schemaless (
   -- * Terms without an associated schema
   Term(..), Field(..), FieldValue(..)
@@ -12,8 +13,11 @@ module Mu.Schema.Schemaless (
 , ToSchemalessTerm(..), ToSchemalessValue(..)
 ) where
 
+import Control.Applicative ((<|>))
+import Data.List (find)
 import qualified Data.Map as M
 import Data.Proxy
+import Data.SOP
 import qualified Data.Text as T
 import Data.Typeable
 
@@ -54,15 +58,89 @@ fromSchemalessTerm
   => Term -> Maybe t
 fromSchemalessTerm t = fromSchema @_ @_ @sch <$> checkSchema (Proxy @sty) t
 
-class CheckSchema (s :: Schema tn fn) (t :: TypeDef tn fn) where
-  checkSchema' :: Term -> Maybe (S.Term s t)
-
--- TODO: write instances of CheckSchema
-
 class ToSchemalessTerm t where
   toSchemalessTerm  :: t -> Term
 class ToSchemalessValue t where
   toSchemalessValue :: t -> FieldValue
+
+class CheckSchema (s :: Schema tn fn) (t :: TypeDef tn fn) where
+  checkSchema' :: Term -> Maybe (S.Term s t)
+class CheckSchemaFields (s :: Schema tn fn) (fields :: [FieldDef tn fn]) where
+  checkSchemaFields :: [Field] -> Maybe (NP (S.Field s) fields)
+class CheckSchemaEnum (choices :: [fn]) where
+  checkSchemaEnumInt  :: Int -> Maybe (NS Proxy choices)
+  checkSchemaEnumText :: T.Text -> Maybe (NS Proxy choices)
+class CheckSchemaValue (s :: Schema tn fn) (field :: FieldType tn) where
+  checkSchemaValue :: FieldValue -> Maybe (S.FieldValue s field)
+class CheckSchemaUnion (s :: Schema tn fn) (ts :: [FieldType tn]) where
+  checkSchemaUnion :: FieldValue -> Maybe (NS (S.FieldValue s) ts)
+
+instance CheckSchemaFields s fields => CheckSchema s ('DRecord nm fields) where
+  checkSchema' (TRecord fields) = S.TRecord <$> checkSchemaFields fields
+  checkSchema' _ = Nothing
+instance CheckSchemaFields s '[] where
+  checkSchemaFields _ = pure Nil
+instance (KnownName nm, CheckSchemaValue s ty, CheckSchemaFields s rest)
+         => CheckSchemaFields s ('FieldDef nm ty ': rest) where
+  checkSchemaFields fs
+    = do let name = T.pack (nameVal (Proxy @nm))
+         Field _ v <- find (\(Field fieldName _) -> fieldName == name) fs
+         v' <- checkSchemaValue v
+         r' <- checkSchemaFields @_ @_ @s @rest fs
+         return (S.Field v' :* r')
+
+instance CheckSchemaEnum choices => CheckSchema s ('DEnum nm choices) where
+  checkSchema' (TEnum n) = S.TEnum <$> checkSchemaEnumInt n
+  checkSchema' (TSimple (FPrimitive (n :: a)))
+    = case (eqT @a @Int, eqT @a @T.Text, eqT @a @String) of
+        (Just Refl, _, _) -> S.TEnum <$> checkSchemaEnumInt n
+        (_, Just Refl, _) -> S.TEnum <$> checkSchemaEnumText n
+        (_, _, Just Refl) -> S.TEnum <$> checkSchemaEnumText (T.pack n)
+        _ -> Nothing
+  checkSchema' _ = Nothing
+instance CheckSchemaEnum '[] where
+  checkSchemaEnumInt  _ = Nothing
+  checkSchemaEnumText _ = Nothing
+instance (KnownName c, CheckSchemaEnum cs)
+         => CheckSchemaEnum (c ': cs) where
+  checkSchemaEnumInt 0 = Just (Z Proxy)
+  checkSchemaEnumInt n = S <$> checkSchemaEnumInt (n-1)
+  checkSchemaEnumText t
+    | t == T.pack (nameVal (Proxy @c)) = Just (Z Proxy)
+    | otherwise                        = S <$> checkSchemaEnumText t
+
+instance CheckSchemaValue s f => CheckSchema s ('DSimple f) where
+  checkSchema' (TSimple t) = S.TSimple <$> checkSchemaValue t
+  checkSchema' _           = Nothing
+instance CheckSchemaValue s 'TNull where
+  checkSchemaValue FNull = Just S.FNull
+  checkSchemaValue _     = Nothing
+instance Typeable t => CheckSchemaValue s ('TPrimitive t) where
+  checkSchemaValue (FPrimitive (t :: a))
+    = case eqT @a @t of
+        Just Refl -> Just (S.FPrimitive t)
+        Nothing   -> Nothing
+  checkSchemaValue _              = Nothing
+-- TODO: handle enums better by an if with typedef
+instance (CheckSchema s (s :/: t))
+         => CheckSchemaValue s ('TSchematic t) where
+  checkSchemaValue (FSchematic t) = S.FSchematic <$> checkSchema' t
+  checkSchemaValue _ = Nothing
+instance CheckSchemaValue s t => CheckSchemaValue s ('TOption t) where
+  checkSchemaValue (FOption x) = S.FOption <$> traverse checkSchemaValue x
+  checkSchemaValue _           = Nothing
+instance CheckSchemaValue s t => CheckSchemaValue s ('TList t) where
+  checkSchemaValue (FList xs) = S.FList <$> traverse checkSchemaValue xs
+  checkSchemaValue _          = Nothing
+-- TODO: how to deal with maps??
+instance CheckSchemaUnion s ts => CheckSchemaValue s ('TUnion ts) where
+  checkSchemaValue x = S.FUnion <$> checkSchemaUnion x
+
+instance CheckSchemaUnion s '[] where
+  checkSchemaUnion _ = Nothing
+instance (CheckSchemaValue s t, CheckSchemaUnion s ts)
+         => CheckSchemaUnion s (t ': ts) where
+  checkSchemaUnion x = Z <$> checkSchemaValue @_ @_ @s @t x <|> S <$> checkSchemaUnion x
 
 -- Boring instances
 deriving instance Show FieldValue
