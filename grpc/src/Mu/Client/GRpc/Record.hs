500824918e1697fb35ee469c8a65d004bfe98187
Alejandro Serrano <trupill@gmail.com>
2019-10-14 10:32:06 +0200

    Support for prefixes in `buildService` (see #15)

--- grpc/src/Mu/Client/GRpc/Record.hs
+++ grpc/src/Mu/Client/GRpc/Record.hs
@@ -12,31 +12,31 @@ import GHC.TypeLits
 import Mu.Client.GRpc
 import Mu.Rpc
 
-buildService :: forall (s :: Service') t
+buildService :: forall (s :: Service') (p :: Symbol) t
                 (nm :: Symbol) (anns :: [Annotation]) (ms :: [Method Symbol]).
-                (s ~ 'Service nm anns ms, Generic t, BuildService s ms (Rep t))
+                (s ~ 'Service nm anns ms, Generic t, BuildService s p ms (Rep t))
              => GrpcClient -> t
-buildService client = to (buildService' (Proxy @s) (Proxy @ms) client)
+buildService client = to (buildService' (Proxy @s) (Proxy @p) (Proxy @ms) client)
 
-class BuildService (s :: Service') (ms :: [Method Symbol]) (f :: * -> *) where
-  buildService' :: Proxy s -> Proxy ms -> GrpcClient -> f a
+class BuildService (s :: Service') (p :: Symbol) (ms :: [Method Symbol]) (f :: * -> *) where
+  buildService' :: Proxy s -> Proxy p -> Proxy ms -> GrpcClient -> f a
 
-instance BuildService s ms U1 where
-  buildService' _ _ _ = U1
-instance BuildService s ms f => BuildService s ms (D1 meta f) where
-  buildService' ps pms client
-    = M1 (buildService' ps pms client)
-instance BuildService s ms f => BuildService s ms (C1 meta f) where
-  buildService' ps pms client
-    = M1 (buildService' ps pms client)
+instance BuildService s p ms U1 where
+  buildService' _ _ _ _ = U1
+instance BuildService s p ms f => BuildService s p ms (D1 meta f) where
+  buildService' ps ppr pms client
+    = M1 (buildService' ps ppr pms client)
+instance BuildService s p ms f => BuildService s p ms (C1 meta f) where
+  buildService' ps ppr pms client
+    = M1 (buildService' ps ppr pms client)
 instance TypeError ('Text "building a service from sums is not supported")
-         => BuildService s ms (f :+: g) where
+         => BuildService s p ms (f :+: g) where
   buildService' = error "this should never happen"
-instance (BuildService s ms f, BuildService s ms g)
-         => BuildService s ms (f :*: g) where
-  buildService' ps pms client
-    = buildService' ps pms client :*: buildService' ps pms client
-instance (GRpcServiceMethodCall s (s :-->: m) h)
-         => BuildService s ms (S1 ('MetaSel ('Just m) u ss ds) (K1 i h)) where
-  buildService' ps _ client
-    = M1 $ K1 $ gRpcServiceMethodCall ps (Proxy @(s :-->: m)) client
+instance (BuildService s p ms f, BuildService s p ms g)
+         => BuildService s p ms (f :*: g) where
+  buildService' ps ppr pms client
+    = buildService' ps ppr pms client :*: buildService' ps ppr pms client
+instance (m ~ AppendSymbol p x, GRpcServiceMethodCall s (s :-->: x) h)
+         => BuildService s p ms (S1 ('MetaSel ('Just m) u ss ds) (K1 i h)) where
+  buildService' ps _ _ client
+    = M1 $ K1 $ gRpcServiceMethodCall ps (Proxy @(s :-->: x)) client
