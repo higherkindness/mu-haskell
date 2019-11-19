7815d964957f0d235b88e05103747daca2d9a8fc
Flavio Corpa <flavio.corpa@47deg.com>
2019-11-19 13:17:40 +0100

    Fix .proto and implement Definition! 📚

--- examples/todolist/src/Definition.hs
+++ examples/todolist/src/Definition.hs
@@ -1,13 +1,54 @@
-{-# language TemplateHaskell #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
 
 module Definition where
 
-import GHC.Generics
-import Data.Hashable
-import Data.Int
-import Data.Text as T
+import           Data.Int
+import           Data.Text     as T
+import           GHC.Generics
 
-import Mu.Schema
-import Mu.Quasi.GRpc
+import           Mu.Quasi.GRpc
+import           Mu.Schema
 
 $(grpc "TodoListSchema" id "todolist.proto")
+
+newtype MessageId =
+  MessageId
+    { value :: Int32
+    }
+  deriving (Eq, Show, Ord, Generic, HasSchema TodoListSchema "MessageId")
+
+data TodoListMessage =
+  TodoListMessage
+    { id, tagId :: Int32
+    , title     :: T.Text
+    }
+  deriving (Eq, Show, Ord, Generic, HasSchema TodoListSchema "TodoListMessage")
+
+data TodoListRequest =
+  TodoListRequest
+    { title :: T.Text
+    , tagId :: Int32
+    }
+  deriving (Eq, Show, Ord, Generic, HasSchema TodoListSchema "TodoListRequest")
+
+newtype TodoListList =
+  TodoListList
+    { list :: [TodoListMessage]
+    }
+  deriving (Eq, Show, Ord, Generic, HasSchema TodoListSchema "TodoListList")
+
+newtype TodoListResponse =
+  TodoListResponse
+    { msg :: TodoListMessage
+    }
+  deriving (Eq, Show, Ord, Generic, HasSchema TodoListSchema "TodoListResponse")
