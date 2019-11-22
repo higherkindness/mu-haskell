4e1de657fed846fe1b22fc188a18cd20cdc5fea4
Flavio Corpa <flavio.corpa@47deg.com>
2019-11-22 14:24:51 +0100

    Finish server! 💻

--- examples/todolist/src/Server.hs
+++ examples/todolist/src/Server.hs
@@ -8,7 +8,6 @@ import           Control.Concurrent.STM
 import           Data.Int
 import           Data.List              (find)
 import           Data.Maybe             (fromMaybe)
-import qualified Data.Text              as T
 
 import           Mu.GRpc.Server
 import           Mu.Server
@@ -20,25 +19,25 @@ main = do
   putStrLn "running todolist application"
   todoId <- newTVarIO 0
   todolist <- newTVarIO []
-  pure ()
-  -- runGRpcApp 8080 (server m todolist)
+  runGRpcApp 8080 (server todoId todolist)
 
 -- Server implementation
--- https://github.com/higherkindness/mu/blob/master/modules/examples/todolist/server/src/main/scala/handlers/TodoListRpcServiceHandler.scala
-type Ids = TVar Int32
+-- https://github.com/frees-io/freestyle/blob/master/modules/examples/todolist-lib/src/main/scala/todo/service/TodoListService.scala
+
+type Id = TVar Int32
 type TodoList = TVar [TodoListMessage]
 
--- server :: TodoList -> ServerIO TodoListService _
--- server i t = Server
---   (reset t :<|>: insert i t :<|>: retrieve m _ :<|>: list_ m :<|>: update m _ :<|>: destroy _ :<|>: H0)
+server :: Id -> TodoList -> ServerIO TodoListService _
+server i t = Server
+  (reset t :<|>: insert i t :<|>: retrieve t :<|>: list_ t :<|>: update t :<|>: destroy t :<|>: H0)
 
 reset :: TodoList -> IO MessageId
 reset t = do
   putStrLn "reset"
   atomically $ writeTVar t []
-  pure $ MessageId 0 -- this means nothing
+  pure $ MessageId 0 -- returns nothing
 
-insert :: Ids -> TodoList -> TodoListRequest -> IO TodoListResponse
+insert :: Id -> TodoList -> TodoListRequest -> IO TodoListResponse
 insert oldId t (TodoListRequest titl tgId) = do
   putStr "insert: " >> print (titl, tgId)
   atomically $ do
@@ -48,12 +47,17 @@ insert oldId t (TodoListRequest titl tgId) = do
     modifyTVar t (newTodo:)
     pure $ TodoListResponse newTodo
 
-retrieve :: TodoList -> MessageId -> IO (Maybe TodoListResponse)
+getMsg :: Int32 -> TodoListMessage -> Bool
+getMsg val (TodoListMessage idM _ _) = idM == val
+
+retrieve :: TodoList -> MessageId -> IO TodoListResponse
 retrieve t (MessageId idMsg) = do
   putStr "retrieve: " >> print idMsg
   atomically $ do
     todos <- readTVar t
-    pure $ TodoListResponse <$> find (\(TodoListMessage idM _ _) -> idM == idMsg) todos
+    pure $ TodoListResponse $ fromMaybe
+      (TodoListMessage 0 0 "I don't know") -- FIXME: we should do something better here!
+      (find (getMsg idMsg) todos)
 
 list_ :: TodoList -> IO TodoListList
 list_ t = do
@@ -63,19 +67,18 @@ list_ t = do
     pure $ TodoListList todos
 
 update :: TodoList -> TodoListMessage -> IO TodoListResponse
-update t msg@(TodoListMessage idM titM tgM) = do
+update t mg@(TodoListMessage idM titM tgM) = do
   putStr "update: " >> print (idM, titM, tgM)
   atomically $ do
-    todos <- readTVar t
-    modifyTVar t (fmap (\ms@(TodoListMessage idMsg _ _) -> if idM == idMsg then msg else ms))
-    pure $ TodoListResponse msg
+    modifyTVar t (fmap (\m -> if getMsg idM m then mg else m))
+    pure $ TodoListResponse mg
 
 destroy :: TodoList -> MessageId -> IO MessageId
 destroy t (MessageId idMsg) = do
   putStr "destroy: " >> print idMsg
   atomically $ do
     todos <- readTVar t
-    case find (\(TodoListMessage idM _ _) -> idM == idMsg) todos of
+    case find (getMsg idMsg) todos of
       Just todo -> do
         modifyTVar t (filter (/=todo))
         pure $ MessageId idMsg -- OK ✅
