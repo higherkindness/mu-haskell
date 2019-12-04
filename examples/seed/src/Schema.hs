bdce1e5d84732763381a8b31bb3c3c0dfdc45c71
Flavio Corpa <flaviocorpa@gmail.com>
2019-12-04 08:31:11 +0100

    Add Seed example 🌱 (#27)

---
+++ examples/seed/src/Schema.hs
@@ -0,0 +1,34 @@
+{-# language DataKinds             #-}
+{-# language DeriveAnyClass        #-}
+{-# language DeriveGeneric         #-}
+{-# language DuplicateRecordFields #-}
+{-# language FlexibleContexts      #-}
+{-# language FlexibleInstances     #-}
+{-# language MultiParamTypeClasses #-}
+{-# language PolyKinds             #-}
+{-# language TemplateHaskell       #-}
+{-# language TypeOperators         #-}
+
+module Schema where
+
+import           Data.Int      (Int32)
+import           Data.Text     as T
+import           GHC.Generics
+
+import           Mu.Quasi.GRpc
+import           Mu.Schema
+
+grpc "SeedSchema" id "seed.proto"
+
+data Person = Person
+  { name :: T.Text
+  , age  :: Int32
+  } deriving (Eq, Show, Ord, Generic, HasSchema SeedSchema "Person")
+
+newtype PeopleRequest = PeopleRequest
+  { name :: T.Text
+  } deriving (Eq, Show, Ord, Generic, HasSchema SeedSchema "PeopleRequest")
+
+newtype PeopleResponse = PeopleResponse
+  { person :: Person
+  } deriving (Eq, Show, Ord, Generic, HasSchema SeedSchema "PeopleResponse")
