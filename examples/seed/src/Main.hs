0ef0f9f7f4d7d97e434aa2b2ce241bb4c62f2f49
Alejandro Serrano <trupill@gmail.com>
2019-12-05 14:03:10 +0100

    Add support for logging and other transformers (#35)
    Fixes #32

--- examples/seed/src/Main.hs
+++ examples/seed/src/Main.hs
@@ -1,11 +1,16 @@
+{-# language FlexibleContexts      #-}
+{-# language OverloadedStrings     #-}
 {-# language PartialTypeSignatures #-}
+{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
 
 module Main where
 
 import           Control.Concurrent       (threadDelay)
 import           Control.Monad.IO.Class   (liftIO)
+import           Control.Monad.Logger
 import           Data.Conduit
 import           Data.Conduit.Combinators as C
+import           Data.Text                as T
 import           Mu.GRpc.Server
 import           Mu.Server
 
@@ -14,25 +19,27 @@ import           Schema
 main :: IO ()
 main = do
   putStrLn "running seed application"
-  runGRpcApp 8080 server
+  runGRpcAppTrans 8080 runStderrLoggingT server
 
 -- Server implementation
 -- https://github.com/higherkindness/mu/blob/master/modules/examples/seed/server/modules/process/src/main/scala/example/seed/server/process/ProtoPeopleServiceHandler.scala
 
-server :: ServerIO PeopleService _
+server :: (MonadServer m, MonadLogger m) => ServerT PeopleService m _
 server = Server (getPerson :<|>: getPersonStream :<|>: H0)
 
 evolvePerson :: PeopleRequest -> PeopleResponse
 evolvePerson (PeopleRequest n) = PeopleResponse $ Person n 18
 
-getPerson :: PeopleRequest -> ServerErrorIO PeopleResponse
-getPerson = return . evolvePerson
+getPerson :: Monad m => PeopleRequest -> m PeopleResponse
+getPerson = pure . evolvePerson
 
-getPersonStream :: ConduitT () PeopleRequest ServerErrorIO ()
-                -> ConduitT PeopleResponse Void ServerErrorIO ()
-                -> ServerErrorIO ()
+getPersonStream :: (MonadServer m, MonadLogger m)
+                => ConduitT () PeopleRequest m ()
+                -> ConduitT PeopleResponse Void m ()
+                -> m ()
 getPersonStream source sink = runConduit $ source .| C.mapM reStream .| sink
   where
     reStream req = do
       liftIO $ threadDelay (2 * 1000 * 1000) -- 2 sec
-      return $ evolvePerson req
+      logDebugN $ T.pack $ "stream request: " ++ show req
+      pure $ evolvePerson req
