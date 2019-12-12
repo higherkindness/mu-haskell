fefc06e8166bac4f1a1dab16d908f62f660ff4d3
Flavio Corpa <flavio.corpa@47deg.com>
2019-12-13 00:05:27 +0100

    pointfreeee 💸

--- examples/with-persistent/src/Server.hs
+++ examples/with-persistent/src/Server.hs
@@ -6,10 +6,10 @@
 
 module Main where
 
-import           Control.Monad.IO.Class       (liftIO)
+import           Control.Monad.IO.Class  (liftIO)
 import           Control.Monad.Logger
 import           Data.Conduit
-import qualified Data.Text                    as T
+import qualified Data.Text               as T
 import           Database.Persist.Sqlite
 import           Mu.GRpc.Server
 import           Mu.Server
@@ -19,7 +19,6 @@ import           Schema
 main :: IO ()
 main = do
   putStrLn "running app with persistent"
-  -- thou shalt not use pools with :memory:
   runStderrLoggingT $
     withSqliteConn @(LoggingT IO) ":memory:" $ \conn -> do
       liftIO $ flip runSqlPersistM conn $ runMigration migrateAll
@@ -28,29 +27,23 @@ main = do
 server :: SqlBackend -> ServerT PersistentService ServerErrorIO _
 server p = Server (getPerson p :<|>: newPerson p :<|>: allPeople p :<|>: H0)
 
-runDb conn f = liftIO $ runSqlPersistM f conn
+runDb = (liftIO .) . flip runSqlPersistM
 
-getPerson :: SqlBackend
-          -> PersonRequest
-          -> ServerErrorIO (Entity Person)
+getPerson :: SqlBackend -> PersonRequest -> ServerErrorIO (Entity Person)
 getPerson conn (PersonRequest idf) = do
   r <- runDb conn $ do
-         let pId = PersonKey $ SqlBackendKey idf
-         maybePerson <- get pId
-         pure $ Entity pId <$> maybePerson
+    let pId = PersonKey $ SqlBackendKey idf
+    maybePerson <- get pId
+    pure $ Entity pId <$> maybePerson
   case r of
     Just p  -> pure p
     Nothing -> serverError $ ServerError NotFound "unknown person"
 
-newPerson :: SqlBackend
-          -> Entity Person
-          -> ServerErrorIO PersonRequest
+newPerson :: SqlBackend -> Entity Person -> ServerErrorIO PersonRequest
 newPerson conn (Entity _ p@(Person name _)) = runDb conn $ do
   PersonKey (SqlBackendKey nId) <- insert p
   pure $ PersonRequest nId
 
-allPeople :: SqlBackend
-          -> ConduitT (Entity Person) Void ServerErrorIO ()
-          -> ServerErrorIO ()
+allPeople :: SqlBackend -> ConduitT (Entity Person) Void ServerErrorIO () -> ServerErrorIO ()
 allPeople conn sink = runDb conn $
   runConduit $ selectSource [] [] .| liftServerConduit sink
