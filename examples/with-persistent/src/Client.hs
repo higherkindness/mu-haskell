5e3aba51555dd11d372a2792c3a5b82e265f708d
Flavio Corpa <flavio.corpa@47deg.com>
2019-12-16 10:43:43 +0100

    Implement add new person 👨🏼‍💻

--- examples/with-persistent/src/Client.hs
+++ examples/with-persistent/src/Client.hs
@@ -11,7 +11,7 @@ module Main where
 import           Data.Conduit
 import qualified Data.Conduit.Combinators as C
 import           Data.Int                 (Int64)
--- import qualified Data.Text                as T
+import qualified Data.Text                as T
 import           Database.Persist.Types   (Entity)
 import           System.Environment
 
@@ -20,16 +20,15 @@ import           Mu.GRpc.Client.TyApps
 import           Schema
 
 main :: IO ()
-main = do -- Setup the client
+main = do
   let config = grpcClientConfigSimple "127.0.0.1" 1234 False
   Right client <- setupGrpcClient' config
-  -- Execute command
   args <- getArgs
   case args of
-    ["watch"]    -> watching client
-    ["get", idP] -> get client idP
-    -- ["add", name, age] -> add client name age
-    _            -> putStrLn "unknown command"
+    ["watch"]          -> watching client
+    ["get", idP]       -> get client idP
+    ["add", name, age] -> add client name age
+    _                  -> putStrLn "unknown command"
 
 get :: GrpcClient -> String -> IO ()
 get client idPerson = do
@@ -39,16 +38,13 @@ get client idPerson = do
     <- gRpcCall @PersistentService @"getPerson" client req
   putStrLn ("GET: response was: " ++ show rknown)
 
--- add :: GrpcClient -> String -> String -> IO ()
--- add client who newstatus = do
---   let hcm = HealthCheckMsg (T.pack who)
---   -- putStrLn ("UNARY: Setting " <> who <> " service to " <> newstatus)
---   r <- gRpcCall @PersistentService @"setStatus" client
---                 (HealthStatusMsg hcm (PersonRequest (T.pack newstatus)))
---   -- putStrLn ("UNARY: Was setting successful? " <> show r)
---   rstatus :: GRpcReply PersonRequest
---     <- gRpcCall @PersistentService @"check" client hcm
---   putStrLn ("UNARY: Checked the status of " <> who <> ". Obtained: " <> show rstatus)
+add :: GrpcClient -> String -> String -> IO ()
+add client name age = do
+  let p = Person (T.pack name) (read age)
+  putStrLn ("ADD: Creating new person " <> name <> " with age " <> age)
+  response :: GRpcReply PersonRequest
+    <- gRpcCall @PersistentService @"newPerson" client p
+  putStrLn ("ADD: Was creating successful? " <> show response)
 
 watching :: GrpcClient -> IO ()
 watching client = do
