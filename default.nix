94c883812cc01cf623dabfc84e50de3a9cc07863
Flavio Corpa <flavio.corpa@47deg.com>
2019-12-20 16:05:50 +0100

    try building mu-grp-server

--- default.nix
+++ default.nix
@@ -1,5 +1,21 @@
-{ pkgs ? import <nixpkgs> (import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz))
+{ nixpkgs ? (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz)
+, system ? builtins.currentSystem
 }:
-  pkgs.haskell-nix.stackProject {
-    src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
-  }
+
+let
+  haskellnix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz);
+  overlay = _: pkgs:
+    let
+      hnPkgs = pkgs.haskell-nix.stackProject {
+        src = ./.;
+        #inherit stackYaml;
+        modules = [];
+      };
+    in {
+      mu-grpc-server = hnPkgs.mu-grpc-server.components.all;
+    };
+in (import nixpkgs {
+  overlays = haskellnix.overlays ++ [ overlay ];
+  config = haskellnix.config // {};
+  inherit system;
+}).mu-grpc-server
