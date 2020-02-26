a5131336895ee0037f47392a05df452d680a7946
Flavio Corpa <flavio.corpa@47deg.com>
2020-02-26 10:29:00 +0100

    Update mu-haskell to use avro-parser-haskell-0.1.1 (#112)

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
 { nixpkgs ? (fetchTarball https://github.com/NixOS/nixpkgs/archive/484ea7bbac5bf7f958b0bb314a3c130b6c328800.tar.gz)
-, pkgs ? import nixpkgs (import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/659d80f.tar.gz))
+, pkgs ? import nixpkgs (import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/21c5528.tar.gz))
 }:
 
 let
   hnPkgs = pkgs.haskell-nix.stackProject {
-   src = ./.;
-   modules = [];
+    src = ./.;
+    modules = [];
   };
 in {
   compendium-client = hnPkgs.compendium-client.components.library;
@@ -17,13 +17,13 @@ in {
   mu-example-seed-protobuf = hnPkgs.mu-example-seed-protobuf.components.all;
   mu-example-todolist = hnPkgs.mu-example-todolist.components.all;
   mu-example-with-persistent = hnPkgs.mu-example-with-persistent.components.all;
-  mu-grpc-common = hnPkgs.mu-grpc-common.components.library;
   mu-grpc-client = hnPkgs.mu-grpc-client.components.library;
+  mu-grpc-common = hnPkgs.mu-grpc-common.components.library;
   mu-grpc-server = hnPkgs.mu-grpc-server.components.all;
+  mu-kafka = hnPkgs.mu-kafka.components.library;
+  mu-optics = hnPkgs.mu-optics.components.library;
   mu-persistent = hnPkgs.mu-persistent.components.library;
   mu-protobuf = hnPkgs.mu-protobuf.components.all;
   mu-rpc = hnPkgs.mu-rpc.components.library;
   mu-schema = hnPkgs.mu-schema.components.library;
-  mu-optics = hnPkgs.mu-optics.components.library;
-  mu-kafka = hnPkgs.mu-kafka.components.library;
 }
