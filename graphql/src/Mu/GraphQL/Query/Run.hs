5e0b0fc0e3d46ace9af0d5df2d2e79d37fe6e416
Alejandro Serrano <trupill@gmail.com>
2020-02-27 10:19:10 +0100

    Start working on running stuff

--- graphql/src/Mu/GraphQL/Query/Run.hs
+++ graphql/src/Mu/GraphQL/Query/Run.hs
@@ -1,19 +1,48 @@
-{-# language DataKinds           #-}
-{-# language GADTs               #-}
-{-# language PolyKinds           #-}
-{-# language ScopedTypeVariables #-}
-{-# language TypeOperators       #-}
+{-# language DataKinds             #-}
+{-# language FlexibleInstances     #-}
+{-# language GADTs                 #-}
+{-# language MultiParamTypeClasses #-}
+{-# language PolyKinds             #-}
+{-# language ScopedTypeVariables   #-}
+{-# language TypeOperators         #-}
+{-# language UndecidableInstances  #-}
 module Mu.GraphQL.Query.Run where
 
 import           Control.Monad.Writer
 import qualified Data.Aeson                  as Aeson
 import           Data.Functor.Identity
+import           GHC.TypeLits
+import           Mu.Rpc
+import           Mu.Schema
 import           Mu.Server
 
 import           Mu.GraphQL.Query.Definition
 
 -- TODO: run the query
-runQuery :: ServerT Identity chn p ServerErrorIO hs
-         -> ServiceQuery p s
-         -> WriterT ServerError IO Aeson.Value
-runQuery = undefined
+runQuery
+  :: RunQueryFindHandler ss s
+  => ServerT Identity chn ('Package pname ss) ServerErrorIO hs
+  -> ServiceQuery ('Package pname ss) s
+  -> WriterT ServerError IO Aeson.Value
+runQuery whole@(Services ss) = runQueryFindHandler whole ss
+
+class RunQueryFindHandler ss s where
+  runQueryFindHandler
+    :: ServerT Identity chn ('Package pname wholess) ServerErrorIO whole
+    -> ServicesT Identity chn ss ServerErrorIO hs
+    -> ServiceQuery ('Package pname ss) s
+    -> WriterT ServerError IO Aeson.Value
+
+instance TypeError ('Text "Could not find handler for " ':<>: 'ShowType s)
+         => RunQueryFindHandler '[] s where
+  runQueryFindHandler = error "this should never be called"
+instance {-# OVERLAPS #-}
+         RunQueryFindHandler (s ': ss) s where
+  runQueryFindHandler whole (this :<&>: _) = runQueryService whole this
+
+runQueryService
+  :: ServerT Identity chn ('Package pname ss) ServerErrorIO hss  -- the whole package
+  -> HandlersT w chn (MappingRight chn sname) methods m hs  -- the one for the service
+  -> ServiceQuery p ('Service sname sanns methods)
+  -> WriterT ServerError IO Aeson.Value
+runQueryService = undefined
