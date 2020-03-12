2796a09c83e092297b17c904bc66ac7407618ca4
Flavio Corpa <flavio.corpa@47deg.com>
2020-03-12 13:33:07 +0100

    Add GraphQL queries for all authors, books and find book by title 📚

--- graphql/exe/Main.hs
+++ graphql/exe/Main.hs
@@ -14,7 +14,7 @@ module Main where
 import           Data.List         (find)
 import           Data.Maybe        (fromMaybe)
 import           Data.Proxy
-import           Data.Text         (Text)
+import           Data.Text         (Text, toCaseFold)
 
 import           Mu.GraphQL.Server
 import           Mu.Rpc
@@ -22,7 +22,9 @@ import           Mu.Schema
 import           Mu.Server
 
 main :: IO ()
-main = runGraphQLApp 8080 libraryServer (Proxy @"Query") (Proxy @"Mutation")
+main = do
+  putStrLn "starting GraphQL server on port 8080"
+  runGraphQLApp 8080 libraryServer (Proxy @"Query") (Proxy @"Mutation")
 
 type ServiceDefinition
   = 'Package ('Just "library")
@@ -40,6 +42,13 @@ type ServiceDefinition
          '[ ObjectField "author" '[]
               '[ 'ArgSingle ('Just "name") '[] ('PrimitiveRef Text)]
               ('RetSingle ('OptionalRef ('ObjectRef "Author")))
+          , ObjectField "book" '[]
+              '[ 'ArgSingle ('Just "title") '[] ('PrimitiveRef Text)]
+              ('RetSingle ('ListRef ('ObjectRef "Book")))
+          , ObjectField "authors" '[]
+              '[] ('RetSingle ('ListRef ('ObjectRef "Author")))
+          , ObjectField "books" '[]
+              '[] ('RetSingle ('ListRef ('ObjectRef "Book")))
           ]
       , Object "Mutation" '[] '[]
       ]
@@ -51,33 +60,49 @@ type ServiceMapping = '[
 
 library :: [(Integer, Text, [(Integer, Text)])]
 library
-  = [ (1, "alex", [(1, "haskell is nice"), (2, "haskell is cool")])
-    , (2, "kant", [(3, "critique of pure reason")])]
+  = [ (1, "Robert Louis Stevenson", [(1, "Treasure Island"), (2, "Strange Case of Dr Jekyll and Mr Hyde")])
+    , (2, "Immanuel Kant", [(3, "Critique of Pure Reason")])
+    , (3, "Michael Ende", [(4, "The Neverending Story"), (5, "Momo")])
+    ]
 
 libraryServer :: forall m. (MonadServer m) => ServerT ServiceMapping ServiceDefinition m _
 libraryServer
   = Services $ (bookId :<||>: bookTitle :<||>: bookAuthor :<||>: H0)
                :<&>: (authorId :<||>: authorName :<||>: authorBooks :<||>: H0)
-               :<&>: (noContext findAuthor :<||>: H0)
+               :<&>: (noContext findAuthor
+                      :<||>: noContext findBookTitle
+                      :<||>: noContext allAuthors
+                      :<||>: noContext allBooks
+                      :<||>: H0)
                :<&>: H0 :<&>: S0
-  where bookId (_, bid) = pure bid
-        bookTitle (aid, bid)
-          = case find (\(aid', _, _) -> aid == aid') library of
-              Nothing            -> pure ""
-              Just (_, _, books) -> pure $ fromMaybe "" (lookup bid books)
-        bookAuthor (aid, _) = pure aid
+  where
+    findBook i = find (\(id', _, _) -> i == id') library
 
-        authorId = pure
-        authorName aid
-          = case find (\(aid', _, _) -> aid == aid') library of
-              Nothing            -> pure ""
-              Just (_, aname, _) -> pure aname
-        authorBooks aid
-          = case find (\(aid', _, _) -> aid == aid') library of
-              Nothing            -> pure []
-              Just (_, _, books) -> pure $ map ((aid, ) . fst) books
+    bookId (_, bid) = pure bid
+    bookTitle (aid, bid)
+      = case findBook aid of
+          Nothing            -> pure ""
+          Just (_, _, books) -> pure $ fromMaybe "" (lookup bid books)
+    bookAuthor (aid, _) = pure aid
 
-        findAuthor aname
-          = case find (\(_, aname', _) -> aname == aname') library of
-              Nothing          -> pure Nothing
-              Just (aid, _, _) -> pure $ Just aid
+    authorId = pure
+    authorName aid
+      = case findBook aid of
+          Nothing            -> pure ""
+          Just (_, aname, _) -> pure aname
+    authorBooks aid
+      = case findBook aid of
+          Nothing            -> pure []
+          Just (_, _, books) -> pure $ map ((aid,) . fst) books
+
+    findAuthor aname
+      = case find (\(_, aname', _) -> toCaseFold aname == toCaseFold aname') library of
+          Nothing          -> pure Nothing
+          Just (aid, _, _) -> pure $ Just aid
+
+    findBookTitle title
+      = pure [(aid, bid) | (aid, _, books) <- library, (bid, title') <- books, toCaseFold title == toCaseFold title']
+
+    allAuthors = pure $ (\(x, _, _) -> x) <$> library
+
+    allBooks = pure $ library >>= (\(x, _, ys) -> (x,) . fst <$> ys)
