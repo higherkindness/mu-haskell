0089e78a443253c3af085ada6854236dc110d002
Alejandro Serrano <trupill@gmail.com>
2020-03-13 08:52:25 +0100

    Update Nix and Cabal configuration

--- default.nix
+++ default.nix
@@ -10,11 +10,9 @@ let
 in {
   compendium-client = hnPkgs.compendium-client.components.library;
   mu-avro = hnPkgs.mu-avro.components.all;
-  mu-example-health-check-avro = hnPkgs.mu-example-health-check-avro.components.all;
-  mu-example-health-check-protobuf = hnPkgs.mu-example-health-check-protobuf.components.all;
+  mu-example-health-check = hnPkgs.mu-example-health-check-avro.components.all;
   mu-example-route-guide = hnPkgs.mu-example-route-guide.components.all;
-  mu-example-seed-avro = hnPkgs.mu-example-seed-avro.components.all;
-  mu-example-seed-protobuf = hnPkgs.mu-example-seed-protobuf.components.all;
+  mu-example-seed = hnPkgs.mu-example-seed.components.all;
   mu-example-todolist = hnPkgs.mu-example-todolist.components.all;
   mu-example-with-persistent = hnPkgs.mu-example-with-persistent.components.all;
   mu-graphql = hnPkgs.mu-graphql.components.library;
