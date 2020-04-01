09ae8c19bb42bc0b6ed8a591fa7e7104a94a6cb9
Alejandro Serrano <trupill@gmail.com>
2020-04-01 15:30:22 +0200

    Support 'avro' 0.5 (#164)
    Co-authored-by: Flavio Corpa <flavio.corpa@47deg.com>

--- adapter/avro/src/Data/Time/Millis.hs
+++ adapter/avro/src/Data/Time/Millis.hs
@@ -2,37 +2,28 @@
 module Data.Time.Millis where
 
 
-import           Control.DeepSeq         (NFData)
-import           Data.Avro.Encode
-import           Data.Avro.EncodeRaw
-import           Data.Avro.FromAvro
+import           Control.DeepSeq             (NFData)
+import           Data.Avro.Encoding.FromAvro
+import           Data.Avro.Encoding.ToAvro
 import           Data.Avro.HasAvroSchema
-import           Data.Avro.Schema        as S
-import           Data.Avro.ToAvro
-import           Data.Avro.Types         as T
+import qualified Data.Avro.Schema.Schema     as S
+import           Data.Int                    (Int32)
 import           Data.Tagged
 import           Data.Time
-import           Unsafe.Coerce
 
 -- | Wrapper for time difference expressed in milliseconds
 newtype DiffTimeMs = DiffTimeMs { unDiffTimeMs :: DiffTime }
   deriving (Show, Eq, Ord, Enum, Num, Fractional, Real, RealFrac, NFData)
 
-instance EncodeAvro DiffTimeMs where
-  avro d
-      -- Unfortunately, the AvroM constructor is not exposed :(
-    = unsafeCoerce ( encodeRaw (fromIntegral $ diffTimeToMillis d :: Int)
-                   , S.Long (Just TimeMicros) )
-
 instance HasAvroSchema DiffTimeMs where
-  schema = Tagged $ S.Int (Just TimeMillis)
+  schema = Tagged $ S.Int (Just S.TimeMillis)
 
 instance ToAvro DiffTimeMs where
-  toAvro = T.Int . fromIntegral . diffTimeToMillis
+  toAvro s = toAvro s . (fromIntegral :: Integer -> Int32) . diffTimeToMillis
 
 instance FromAvro DiffTimeMs where
-  fromAvro (T.Int  v) = pure $ millisToDiffTime (toInteger v)
-  fromAvro v          = badValue v "TimeMicros"
+  fromAvro (Int _ v) = pure $ millisToDiffTime (toInteger v)
+  fromAvro _         = Left "expecting time_ms"
 
 diffTimeToMillis :: DiffTimeMs -> Integer
 diffTimeToMillis = (`div` 1000000000) . diffTimeToPicoseconds . unDiffTimeMs
