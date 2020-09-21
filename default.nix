720ed7ee50a1583443cce2991d5da08746961396
Alejandro Serrano <trupill@gmail.com>
2020-09-21 22:40:04 +0200

    Fix, add mu-lense package and update stack.yaml and nix (#228)

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
-  haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/d3edb6e.tar.gz) {};
+  haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/2a15520.tar.gz) {};
   nixpkgsSrc = haskellNix.sources.nixpkgs-2003;
   nixpkgsArgs = haskellNix.nixpkgsArgs;
 in
@@ -14,22 +14,23 @@ let
   };
 in {
   compendium-client = hnPkgs.compendium-client.components.library;
-  mu-avro = hnPkgs.mu-avro.components.all;
-  mu-example-health-check = hnPkgs.mu-example-health-check.components.all;
-  mu-example-library = hnPkgs.mu-example-library.components.all;
-  mu-example-route-guide = hnPkgs.mu-example-route-guide.components.all;
-  mu-example-seed = hnPkgs.mu-example-seed.components.all;
-  mu-example-todolist = hnPkgs.mu-example-todolist.components.all;
-  mu-example-with-persistent = hnPkgs.mu-example-with-persistent.components.all;
+  mu-avro = hnPkgs.mu-avro.components.library;
+  mu-example-health-check = hnPkgs.mu-example-health-check.components.exes;
+  mu-example-library = hnPkgs.mu-example-library.components.exes;
+  mu-example-route-guide = hnPkgs.mu-example-route-guide.components.exes;
+  mu-example-seed = hnPkgs.mu-example-seed.components.exes;
+  mu-example-todolist = hnPkgs.mu-example-todolist.components.exes;
+  mu-example-with-persistent = hnPkgs.mu-example-with-persistent.components.exes;
   mu-graphql = hnPkgs.mu-graphql.components.library;
   mu-grpc-client = hnPkgs.mu-grpc-client.components.library;
   mu-grpc-common = hnPkgs.mu-grpc-common.components.library;
-  mu-grpc-server = hnPkgs.mu-grpc-server.components.all;
+  mu-grpc-server = hnPkgs.mu-grpc-server.components.library;
   mu-kafka = hnPkgs.mu-kafka.components.library;
+  mu-lens = hnPkgs.mu-lens.components.library;
   mu-optics = hnPkgs.mu-optics.components.library;
   mu-persistent = hnPkgs.mu-persistent.components.library;
   mu-prometheus = hnPkgs.mu-prometheus.components.library;
-  mu-protobuf = hnPkgs.mu-protobuf.components.all;
+  mu-protobuf = hnPkgs.mu-protobuf.components.library;
   mu-rpc = hnPkgs.mu-rpc.components.library;
   mu-schema = hnPkgs.mu-schema.components.library;
   mu-tracing = hnPkgs.mu-tracing.components.library;
