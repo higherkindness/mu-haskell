436dce681c7e71417e101b65f5080bd0aae2300f
Alejandro Serrano <trupill@gmail.com>
2020-10-12 14:56:18 +0200

    Prepare release 0.4 (#233)

---
+++ release-package.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cabal sdist ${1}
+cabal upload dist-newstyle/sdist/${1}-${2}.tar.gz
+echo "Check that it has been published correctly, and press Enter"
+read
+cabal upload --publish dist-newstyle/sdist/${1}-${2}.tar.gz
+cabal v2-haddock --builddir="dist-newstyle" --haddock-for-hackage --enable-doc ${1}
+cabal upload -d --publish dist-newstyle/${1}-${2}-docs.tar.gz
