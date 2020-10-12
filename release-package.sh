b8be75e33eaeffe1e4a3d7f14dbbd9acf787cf66
Alejandro Serrano <trupill@gmail.com>
2020-10-12 11:56:39 +0200

    Move release check to the right place

--- release-package.sh
+++ release-package.sh
@@ -2,8 +2,8 @@
 
 cabal sdist ${1}
 cabal upload dist-newstyle/sdist/${1}-${2}.tar.gz
-cabal upload --publish dist-newstyle/sdist/${1}-${2}.tar.gz
 echo "Check that it has been published correctly, and press Enter"
 read
+cabal upload --publish dist-newstyle/sdist/${1}-${2}.tar.gz
 cabal v2-haddock --builddir="dist-newstyle" --haddock-for-hackage --enable-doc ${1}
 cabal upload -d --publish dist-newstyle/${1}-${2}-docs.tar.gz
