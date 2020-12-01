a30a9e21c050bae9f2081799e8bc664fd7137dc2
Alejandro Serrano <trupill@gmail.com>
2020-12-01 10:50:59 +0100

    Maps in Protocol Buffers (#268)
    And various fixes in Avro required for everything to still work.
    
    Co-authored-by: Flavio Corpa <flavio.corpa@47deg.com>

---
+++ test-schema.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "BUILDING"
+stack build mu-avro mu-protobuf
+mkdir -p dist
+
+echo "\nAVRO\n====\n"
+echo "python/generate"
+python3 adapter/avro/test/avro/generate.py adapter/avro/test/avro/example.avsc dist/avro-python.avro
+stack test-avro dist/avro-haskell.avro dist/avro-python.avro
+echo "ptyhon/consume"
+python3 adapter/avro/test/avro/consume.py adapter/avro/test/avro/example.avsc dist/avro-haskell.avro
+
+echo "\nPROTOBUF\n========\n"
+echo "python/generate"
+python2 adapter/protobuf/test/protobuf/generate.py dist/protobuf-python.pbuf
+stack exec test-protobuf dist/protobuf-haskell.pbuf dist/protobuf-python.pbuf
+echo "python/consume"
+python2 adapter/protobuf/test/protobuf/consume.py dist/protobuf-haskell.pbuf
