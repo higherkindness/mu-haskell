bc7232fac6346cdbda27e3db1cd554b481af7988
Alejandro Serrano <trupill@gmail.com>
2021-09-17 21:24:08 +0200

    Update Nix file

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
-  haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/e7961ee.tar.gz) {};
+  haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/5dd8d47.tar.gz) {};
   nixpkgsSrc = haskellNix.sources.nixpkgs-2003;
   nixpkgsArgs = haskellNix.nixpkgsArgs;
 in
